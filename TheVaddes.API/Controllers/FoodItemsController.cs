c03cd8331df6fcbab169bd8397125afba0d7af34
Susheel Raai <raai.susheel@gmail.com>
2022-04-09 19:32:35 +0100

    initial commit

---
+++ TheVaddes.API/Controllers/FoodItemsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TheVaddes.Data.IDAL;
+
+namespace TheVaddes.API.Controllers
+{
+    [RoutePrefix("Api/FoodItems")]
+    public class FoodItemsController : ApiController
+    {
+        private readonly IFoodItemsRepository _FoodItems;
+
+        [HttpGet]
+        [Route("GetAllFoodItemDetails")]
+        public IHttpActionResult GetAllFoodItemDetails()
+        {
+            try
+            {
+                var results = _FoodItems.GetAllFoodItems();
+                return Ok(results);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ TheVaddes.Data/Models/FoodItemsModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheVaddes.Data.Models
+{
+    public class FoodItemsModel
+    {
+        public int itemId   { get; set; }
+        public string itemName { get; set; }    
+        public string itemType { get; set; }    
+        public decimal itemPrice    { get; set; }
+        public string itemImage     { get; set; }
+        public string itemDescription   { get; set; }
+        public int itemReview { get; set; }
+
+    }
+}
